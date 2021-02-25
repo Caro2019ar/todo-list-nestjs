0145cbcdadd73fc3006f7e561f313216e522c550
Totxo <tomaschorroarin@gmail.com>
2021-02-24 22:49:07 -0300

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/tasks.service';
+
+@Module({
+  imports: [],
+  controllers: [
+      TasksController
+  ],
+  providers: [
+      TasksService
+  ],
+})
+export class AppModule {}
